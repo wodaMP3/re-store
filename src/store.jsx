d70fd12ac14c47724404a09eaa411c7f56419601
Artem <bezkorovainy2006@gmail.com>
2024-03-13 19:25:17 +0100

    // TESTED added enhancer-redux

--- src/store.jsx
+++ src/store.jsx
@@ -1,7 +1,25 @@
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 
 import reducer from './reducers';
 
-const store = createStore(reducer);
+
+
+
+const stringEnhancer = (createStore) => (...args) => {
+    const store = createStore(...args);
+    const originalDispatch = store.dispatch;
+    store.dispatch = (action) => {
+
+        if (typeof action === 'string'){
+            return originalDispatch({
+                type: action
+            });
+        }
+        return originalDispatch(action);
+    };
+    return store;
+};
+
+const store = createStore(reducer, stringEnhancer);
 
 export default store;
