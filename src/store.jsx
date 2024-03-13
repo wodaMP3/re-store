e9d33a9a077eb2396a29b5645ca44d7e5f81d7da
Artem <bezkorovainy2006@gmail.com>
2024-03-13 19:54:24 +0100

    updated to middleWare

--- src/store.jsx
+++ src/store.jsx
@@ -1,25 +1,25 @@
-import { createStore, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import reducer from './reducers';
 
 
-
-
-const stringEnhancer = (createStore) => (...args) => {
-    const store = createStore(...args);
-    const originalDispatch = store.dispatch;
-    store.dispatch = (action) => {
-
-        if (typeof action === 'string'){
-            return originalDispatch({
-                type: action
-            });
-        }
-        return originalDispatch(action);
-    };
-    return store;
+const logMiddleware = ({ getState }) => (next) => (action) => {
+    console.log(action.type, getState());
+    return next(action);
 };
 
-const store = createStore(reducer, stringEnhancer);
+const stringMiddleware = () => (next) => (action) => {
+    if (typeof action === 'string'){
+        return next({
+            type: action
+        });
+    }
+    return next(action);
+};
 
+
+const store = createStore(reducer, applyMiddleware(
+    stringMiddleware, logMiddleware));
+
+store.dispatch('HELLO_WORLD')
 export default store;
